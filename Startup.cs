a3f252cac5bdfa91dc24fddc2c32a706e5ec8aca
Joe Dawson <jdawson@Marine.ie>
2019-08-22 14:59:03 +0100

    Changes for libman.

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using EFCore.Playground.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using EFCore.Playground.DataAccess;
 
 namespace EFCore.Playground
 {
